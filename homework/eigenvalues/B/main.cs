44c8c472ba8af278b18405e5a50bbbd57ec70032
ChristianeRahbek <christiane@Christianes-pc.localdomain>
2022-03-30 11:59:15 +0200

    new work on eigenvalues

---
+++ homework/eigenvalues/B/main.cs
@@ -0,0 +1,72 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+class main{
+	public static void Main(){
+		//Making the Hamiltonian matrix
+		(matrix H, vector r, double dr) = makeHamiltonian(20, 10);
+		(matrix H1, vector r1, double dr1) = makeHamiltonian(10, 10);
+		(matrix H2, vector r2, double dr2) = makeHamiltonian(20, 5);
+		
+		//convergence with respct to rmax
+
+
+
+
+
+
+		//Performing eigenvalue decomposition
+		vector e, e1, e2; matrix V, V1, V2;
+		(e, V) = jacobi.cyclic(H);
+		(e1, V1) = jacobi.cyclic(H1);
+		(e2, V2) = jacobi.cyclic(H2);
+
+		//Printing results
+		e.print("Eigenvalues points = 20, dr = 10: ");
+		e1.print("Eigenvalues points = 10, dr = 10: ");
+		e2.print("Eigenvalues points = 20, dr = 5: ");
+
+	}
+
+	public static void rMaxConv() {
+		double maxR = 10;
+		int nOofPoints = 20
+		double[] tabVal_energy = new double[] {}; //FIND TABLE VALUES
+		using(var outfile = new System.IO.StreamWriter("rMaxConv_data")) {
+			for(int i = 0; i < tabVal_energy.Length(); i++) { //checking for all tab values
+				for(double r = 0; r <= maxR; r += 1.0/8) {
+					(matrix H1, vector r1, double dr1) = makeHamiltonian(noOfPoints, r);
+					(vector e, matrix V) = jacobi.cyclic(H1);
+					outfile.WriteLine({r} {e[i]} {tabVal_energy[i]});
+				}
+			}
+		}
+	}
+
+
+	public static (matrix, vector, double) makeHamiltonian(int noOfPoints, double rmax) {
+		double dr = rmax / (noOfPoints + 1);
+		vector r = new vector(noOfPoints);
+		
+		for(int i = 0; i < noOfPoints; i++) {
+			r[i]=dr*(i+1);
+		}
+
+		matrix H = new matrix(noOfPoints,noOfPoints);
+
+		for(int i = 0; i < noOfPoints - 1; i++){
+			matrix.set(H, i, i, -2);
+			matrix.set(H, i, i+1, 1);
+			matrix.set(H, i+1, i, 1);
+		}
+
+		matrix.set(H, noOfPoints - 1, noOfPoints - 1, -2);
+		H*=-0.5/dr/dr;
+
+		for(int i = 0; i < noOfPoints; i++) {
+			H[i,i] += -1 / r[i];
+		}
+		return (H, r, dr);
+	}
+}
