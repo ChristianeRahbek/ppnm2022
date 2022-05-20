737dcb5cb0d16fc95315760b3b640ac5c7a4f171
ChristianeRahbek <christiane@Christianes-pc.localdomain>
2022-05-20 11:46:25 +0200

    Linear equations C done

---
+++ homework/linear_equations/C/main.cs
@@ -0,0 +1,31 @@
+using System;
+using static System.Console;
+using System.Diagnostics;
+
+class main{
+	public static void Main() {
+		int N = 300;
+		var rand = new Random();
+
+		for(int n = 10; n <= N; n += 10) {
+			var A = new matrix(n, n);
+
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < n; j++) {
+					A[i, j] = rand.Next();
+				}
+			}
+
+			var timer = new Stopwatch();
+
+			timer.Start();
+			var qrgs = new QRGS(A);
+			timer.Stop();
+
+			var timeElapsed = timer.ElapsedTicks; //Milliseconds;
+
+			WriteLine($"{n} {timeElapsed}");
+
+		}
+	}
+}
